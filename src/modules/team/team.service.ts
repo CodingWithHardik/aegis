2484161dacc196a0929cf106a34931cff65fd2c2
Hardik Gupta <hardikgupta0002@gmail.com>
2026-09-13 01:56:10 +0530

    fix(team): upper role permission

--- src/modules/team/team.service.ts
+++ src/modules/team/team.service.ts
@@ -77,6 +77,8 @@ export class TeamService {
                 throw new AppError("Unauthorized", 403);
             if (getUserRole.role !== "SUPER_ADMIN" && getUserRole.role !== "ADMIN")
                 throw new AppError("Unauthorized", 403);
+            if (user.id === data.userId && data.role === "SUPER_ADMIN" && getUserRole.role !== "SUPER_ADMIN")
+                throw new AppError("Unauthorized", 403);
         }
 
         const result = await this.teamRepo.updateTeam(
